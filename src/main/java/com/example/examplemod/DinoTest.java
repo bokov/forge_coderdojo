09b84e5063eb39c7634c84660cfdcac0946e3ab9
Alex Bokov <bokov@uthscsa.edu>
2020-10-04 11:25:18 -0500

    DinoTest.java,Dinosaur.java; test; Testing running of individual classes

---
+++ src/main/java/com/example/examplemod/DinoTest.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+
+public class DinoTest {
+	public static void main(String[] args) {
+		Dinosaur ronin = new Dinosaur("Ronin");
+		System.out.println("Ronin's name: " + ronin.name);
+		ronin.roar();
+	}
+	
+}
+
---
+++ src/main/java/com/example/examplemod/Dinosaur.java
@@ -0,0 +1,16 @@
+package com.example.examplemod;
+
+public class Dinosaur {
+	public String name;
+	
+	public Dinosaur(String name) {
+		this.name = name;
+	}
+	
+	public void roar() {
+		System.out.println("Rrrrawwr");
+	}
+}
+
+
+
